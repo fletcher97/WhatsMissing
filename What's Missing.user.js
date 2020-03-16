0719c12c276308e816f509d843e9e0cadcaf27a4
BuildTools <unconfigured@null.spigotmc.org>
2020-03-16 00:59:36 +0000

    - Messages to user no long use console but alerts - Fixed error in calculating number of videos not in the current save - Instruction inFile have been removed - Method documentation updated - Style of 'powered by' update to adjust to dark mode

--- What's Missing.user.js
+++ What's Missing.user.js
@@ -15,32 +15,6 @@
 // @grant        GM_deleteValue
 // ==/UserScript==
 
-//
-//  This program was made in a day and may contain bugs. It's not been optimized and could
-//  be much better but at least it gets the job done. Feel free to change it any way you see fit.
-//
-//  To use this programe make sure you are in a playlist's page and not a video page watching a playlist.
-//  The buttons should appear above the discription. (They haven't been styled and should be obvious).
-//  If they don't appear refresh the page. Make sure the script is installed correctly and active.
-//
-//Intructions:
-//  SAVE:
-//    1. Open youtube and navigate to the desired playlist you wish to save.
-//    2. Load the full playlist (scroll down to the last video on the playlist).
-//       If you don't scroll down only the visible video will be saved (increments of 100)
-//    3. Click save/update button and confirm.
-//  DELETE:
-//    1. Open youtube and navigate to the playlist you wish to remove from storage
-//    2. click delete and confirm
-//  CHECK:
-//    1. Open youtube and navigate to the desired playlist you wish to save.
-//    2. Load the full playlist (scroll down to the last video on the playlist).
-//       If you don't scroll down only the visible video will be checked (increments of 100)
-//    3. Click the check button and confirm.
-//    4. To view the results press F12, ctrl+shift+i or right-click and select inspect
-//    5. Select the tab 'Console' at the top and you should see the output.
-//       If you only see 'Check end' no changes were detected
-
 //Verifies if the presented playlist is saved in GreaseMonkey memory
 function containsPL() {
     var url = new URL(window.location.href);
@@ -50,8 +24,6 @@ function containsPL() {
 //Generate a list of titles from the current playlist (playlist page)
 //event - catcher parameter for when the method is called through the listenner
 //returnRes - wether the result should be returned or saved. (0 = save result; 1 = return result)
-//p - playlist name .The pprogram will try to check the playlist name but if it can't find it will use this value
-//    if specified so that the user only has to enter the name of the playlist once per operation.
 function getList(event = null, returnRes = 0){
     //get a list of all the videos
     var list = document.getElementsByClassName('style-scope ytd-playlist-video-list-renderer').contents.querySelectorAll("[id='video-title']");
@@ -59,21 +31,15 @@ function getList(event = null, returnRes = 0){
 	list = Array.from(list, i => i.getAttribute("title").replace(/"/g, '\\"'));
 
     var url = new URL(window.location.href);
-    var pl_id = url.searchParams.get('list')
+    var pl_id = url.searchParams.get('list');
     var saved = GM_getValue(pl_id);
 
     //check if playlist was saved and confirm user intension
-    if(saved === undefined && returnRes !== 1){
-        if(!window.confirm("This playlist hasn't been saved yet. Do you want to save it for the first time?")){
-            console.log('canceled');
-            return;
-        }
-    }else if(returnRes !== 1){
+    if(saved !== undefined && returnRes !== 1){
         var c = Object.keys(JSON.parse(saved)).length;
         if(!window.confirm("This playlist has been saved before and it had " + c +
                           " items. Do you want to overide your previous save?\n" +
                           "New save will store " + list.length + " items.")){
-            console.log('canceled');
             return;
         }
     }
@@ -91,18 +57,15 @@ function getList(event = null, returnRes = 0){
     }
     //save playlist
     GM_setValue(pl_id, json);
-    console.log('saved');
 
     //update visible buttons
-    document.getElementById('savePL').innerHTML = 'Update Save';
+    document.getElementById('savePL').innerHTML = 'Saved!';
     document.getElementById('checkPL').style.visibility = 'visible';
     document.getElementById('deletePL').style.visibility = 'visible';
 }
 
 //Check if there have been any changes to the playlist
 //event - catcher parameter for when the method is called through the listenner
-//p - playlist name .The pprogram will try to check the playlist name but if it can't find it will use this value
-//    if specified so that the user only has to enter the name of the playlist once per operation.
 function checkPL(event = null){
 
     //check if playlist was saved previously and retrieve data
@@ -113,44 +76,49 @@ function checkPL(event = null){
         window.alert('You haven\'t saved this playlist yet.');
         return;
     }
-    window.alert('Check console for a list of changes');
     save = JSON.parse(save);
     var current = getList(null, 1);
     var size = Math.min(Object.keys(save).length, Object.keys(current).length);
 
-    //Check if videos in current playlist were deleted or made private. Log on the console any changes
+	//Check if videos in current playlist were deleted or made private. Log on the console any changes
+	var msg = "The removed videos are:\n";
+	var count = 0;
     for(var i = 0; i < size; i++){
         if(current[i+1] === "[Private video]" || current[i+1] === "[Deleted video]"){
             if(save[i+1] === "[Private video]" || save[i+1] === "[Deleted video]"){
-                console.log("The video at the position " + (i+1) +
-                            " was removed before last save and can't be identified");
+				msg += (i+1) + ": Removed before last save\n";
+				count++;
             }else{
-                console.log("The video at the position " + (i+1) +
-                            " has been removed. The video was \"" + save[i+1] + "\"");
+				msg += (i+1) + ": " + save[i+1] + "\n";
+				count++;
             }
         }
-    }
+	}
+	if(count == 0){
+		window.alert("No removed videos found");
+	}else{
+		window.alert("Total count: " + count + " videos removed\n" + msg);
+	}
 
     //Warn user of mismatch in current/saved playlist sizes
     if(Object.keys(save).length > Object.keys(current).length){
-       console.log("Only the first " + size + " were checked. Some videos were deleted or could not be found." +
-                   " Plz update the save. The last videos on the saved playlist are:");
-       for(var j = size; j < Object.keys(save).length; j++){
-           if(save[j+1] === "[Private video]" || save[j+1] === "[Deleted video]"){continue;}
-           console.log((j+1) + ": \"" + save[j+1] + "\"");
-       }
+		var msg2 = "The playlist has less videos than the save. If no changes have been made, make sure you have scrolled down to the last video.\n" +
+			"If videos have been removed from the middle of the playlist all indeces have shifted and" +
+			" you will get mismatches between the removed videos and it's suggested name.\n" +
+			"If videos have been removed at the end they were:\n";
+		for(var j = size; j < Object.keys(save).length; j++){
+			if(save[j+1] === "[Private video]" || save[j+1] === "[Deleted video]"){continue;}
+			msg2 += (j+1) + ": " + save[j+1] + "\n";
+		}
+		window.alert(msg2);
     }else if(Object.keys(save).length < Object.keys(current).length){
-        console.log("A total of " + (size - Object.keys(current).length) +
-                    " new videos were added since last save starting at position " + size + ".");
+		window.alert("Changes to the playlist size have been detected. The current playlist has " + (Object.keys(current).length - size) +
+			" more videos than the save. Please save your current playlist in order to find missing videos in the future.");
     }
-
-    console.log('Check end');
 }
 
 //Delete a playlist from storage
 //event - catcher parameter for when the method is called through the listenner
-//p - playlist name .The pprogram will try to check the playlist name but if it can't find it will use this value
-//    if specified so that the user only has to enter the name of the playlist once per operation.
 function deletePL(event = null){
     var url = new URL(window.location.href);
     var pl_id = url.searchParams.get('list');
@@ -162,7 +130,6 @@ function deletePL(event = null){
     }
     //confirm playlist save deletion
     if(window.confirm("Are you sure you want to remove this from your saved playlists?")){
-        console.log("deleting:" + pl);
         GM_deleteValue(pl_id);
 
         //update visible buttons
@@ -211,7 +178,7 @@ function setup(){
     save.addEventListener("click", getList);
 
     var end_text = document.createElement('h5');
-    end_text.style = 'margin:5px;';
+    end_text.style = 'margin:5px;color:var(--yt-live-chat-primary-text-color)';
     end_text.innerHTML = "by What's Missing";
 
     buttons_div.appendChild(save);
@@ -239,7 +206,6 @@ var mutationObserver = new MutationObserver(function(mutations) {
 
 //wait for page to load before loading buttons
 window.addEventListener('load', function () {
-    console.log('load');
     if(window.location.href.includes('/playlist')){
        setup();
        mutationObserver.observe(document.getElementsByClassName('style-scope ytd-playlist-sidebar-primary-info-renderer').menu, {childList: true, subtree: true})
@@ -247,7 +213,6 @@ window.addEventListener('load', function () {
 })
 //reload button when moving between youtube 'pages'
 window.addEventListener('yt-navigate-finish', function () {
-    console.log('nav end');
     if(window.location.href.includes('/playlist')){
        setup();
        mutationObserver.observe(document.getElementsByClassName('style-scope ytd-playlist-sidebar-primary-info-renderer').menu, {childList: true, subtree: true})
